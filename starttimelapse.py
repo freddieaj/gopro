3c897817a8536bff206cc3925f22cd6fad63801a
Frederick Jackson <freddieajackson@gmail.com>
2020-07-05 16:48:37 +0100

    Add function to set GoPro to timelapse mode, and start capture.

---
+++ starttimelapse.py
@@ -0,0 +1,5 @@
+def starttimelapse():
+    gpCam.mode(constants.Mode.MultiShotMode, constants.Mode.SubMode.MultiShot.TimeLapse) #change mode to multishot and sub-mode to timelapse
+    gpCam.gpControlSet(constants.Multishot.TIMELAPSE_INTERVAL, constants.Multishot.TimeLapseInterval.I30) #change time lapse interval to 30 seconds
+    print('Timelapse initiated at', datetime.today())
+    gpCam.shutter(constants.start)
