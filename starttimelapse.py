fd5fe63065bd4dba40ffee39502c469c6699508d
Frederick Jackson <freddieajackson@gmail.com>
2020-07-05 17:04:39 +0100

    Add relevant modules

--- starttimelapse.py
+++ starttimelapse.py
@@ -1,3 +1,9 @@
+from datetime import datetime
+from goprocam import GoProCamera
+from goprocam import constants
+import time
+import sys
+
 def starttimelapse():
     gpCam.mode(constants.Mode.MultiShotMode, constants.Mode.SubMode.MultiShot.TimeLapse) #change mode to multishot and sub-mode to timelapse
     gpCam.gpControlSet(constants.Multishot.TIMELAPSE_INTERVAL, constants.Multishot.TimeLapseInterval.I30) #change time lapse interval to 30 seconds
