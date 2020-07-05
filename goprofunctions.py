1321f422ee5b58d9ec408df367c983390b5fd515
Frederick Jackson <freddieajackson@gmail.com>
2020-07-05 20:42:27 +0100

    Aesthetic changes

--- goprofunctions.py
+++ goprofunctions.py
@@ -5,7 +5,6 @@ import time
 import sys
 
 def starttimelapse(gpCam):
-    print('\n')
     gpCam.mode(constants.Mode.MultiShotMode, constants.Mode.SubMode.MultiShot.TimeLapse) #change mode to multishot and sub-mode to timelapse
     gpCam.gpControlSet(constants.Multishot.TIMELAPSE_INTERVAL, constants.Multishot.TimeLapseInterval.I30) #change time lapse interval to 30 seconds
 
