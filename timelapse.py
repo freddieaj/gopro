4a4229ca0d9adca2c3ce90cff13a73e84695392f
Frederick Jackson <freddieajackson@gmail.com>
2020-07-05 21:38:37 +0100

    Added comments

--- timelapse.py
+++ timelapse.py
@@ -11,7 +11,7 @@ print('')
 while True:
     try:
         string = input('specify a delay time in format HH:MM:SS - ').split(':')
-        delay = datetime.timedelta(seconds = int(string[0])*3600 + int(string[1])*60 + int(string[2]))
+        delay = datetime.timedelta(seconds = int(string[0])*3600 + int(string[1])*60 + int(string[2])) #parse input to a timedelta object
 
     except ValueError:
         print("Sorry, I didn't understand that input.")
@@ -22,10 +22,10 @@ while True:
 print('')
 print(datetime.datetime.today().strftime('%H:%M:%S'), ' = current time')
 delay_str = str(delay)
-if len(delay_str) < 8: delay_str = '0'+delay_str
+if len(delay_str) < 8: delay_str = '0'+delay_str #pad timedelta value with a 0 if needed to satisfy my OCD
 print(delay_str, ' = time till target (hh:mm:ss) \n' )
 
 # %% Interface with GoPro API
-gpCam = GoProCamera.GoPro()
-t = Timer(delay.seconds, goprofunctions.starttimelapse, args = [gpCam])
-t.start()
+gpCam = GoProCamera.GoPro() # Connect to GoPro on current network
+t = Timer(delay.seconds, goprofunctions.starttimelapse, args = [gpCam]) # Define a timer that calls the time lapse function
+t.start() # start the timer
